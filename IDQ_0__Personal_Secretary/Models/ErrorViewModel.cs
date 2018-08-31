0e098593e4ad757f492864db847d2ba2583170eb
kibermansha <kibermansha@gmail.com>
2018-08-31 16:16:35 +0300

    fail testing unit

---
+++ IDQ_0__Personal_Secretary/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IDQ_0__Personal_Secretary.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
