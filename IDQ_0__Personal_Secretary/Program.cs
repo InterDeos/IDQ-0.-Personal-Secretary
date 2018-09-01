7473aa73a5f565d5615cec1c18c674bb31b54f88
kibermansha <kibermansha@gmail.com>
2018-09-01 09:02:49 +0300

    Program change

--- IDQ_0__Personal_Secretary/Program.cs
+++ IDQ_0__Personal_Secretary/Program.cs
@@ -14,7 +14,13 @@ namespace IDQ_0__Personal_Secretary
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+            host.Run();
+            //CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
