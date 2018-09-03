09327a7007468b34a52b1862a2889bbe4044cfce
kibermansha <kibermansha@gmail.com>
2018-09-03 09:36:32 +0300

    post get requer test

--- IDQ_0__Personal_Secretary/Controllers/HomeController.cs
+++ IDQ_0__Personal_Secretary/Controllers/HomeController.cs
@@ -6,9 +6,16 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using IDQ_0__Personal_Secretary.Models;
 using IDQ_0__Personal_Secretary.Models.DataBase;
+using IDQ_0__Personal_Secretary.Models.DataBase.Entities;
 
 namespace IDQ_0__Personal_Secretary.Controllers
 {
+    public class PR
+    {
+        public string Name { get; set; }
+        public int Priority { get; set; }
+        public string Description { get; set; }
+    }
     public class HomeController : Controller
     {
         DBContext db;
@@ -19,6 +26,19 @@ namespace IDQ_0__Personal_Secretary.Controllers
         public IActionResult Index()
         {
             return View(db.Projects.ToList());
+        }
+        [HttpGet]
+        public IActionResult Buy(int id)
+        {
+            ViewBag.ProjectId = 0;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(PR project)
+        {
+            db.Projects.Add(new Project(project.Priority, project.Name, project.Description));
+            db.SaveChanges();
+            return $"Project added";
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
